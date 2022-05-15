74ddc26c3a85a80fa2a24c5516e98f2014af6273
Raj_Pakhurde <rajofficial4803@gmail.com>
2022-05-15 23:05:09 +0530

    Added Weather website card

---
+++ script.js
@@ -0,0 +1,21 @@
+const weather = document.querySelector(".weather");
+const heading = document.querySelector(".heading");
+const card = document.querySelector(".card");
+const heart = document.querySelector(".heart");
+const btn = document.querySelector(".btn");
+var colors = ["red", "#abdcff"];
+var colorIndex = 0;
+
+function redheart() {
+  heart.style.color = colors[colorIndex];
+  colorIndex = (colorIndex + 1) % colors.length;
+  console.log(colorIndex);
+}
+
+function openGithub() {
+  open("https://github.com/RajPakhurde/Weather-web-app");
+}
+
+function openLive() {
+  open(" https://rajpakhurde.github.io/Weather-web-app/");
+}
